866e956f33411d439339806da0b52f0f8d623348
Andrew <andrewbg8@gmail.com>
2025-11-25 21:17:53 -0500

    test vscode-diff.nvim

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,24 +1,9 @@
 return {
-
 	{
-		"sindrets/diffview.nvim",
-		lazy = true,
+		"esmuellert/vscode-diff.nvim",
+		dependencies = { "MunifTanjim/nui.nvim" },
 		keys = {
-			{
-				"<leader>gd",
-				function()
-					local success = pcall(vim.cmd, "DiffviewClose")
-					if not success then
-						vim.cmd("DiffviewOpen")
-					end
-				end,
-				desc = "Toggle Diffview",
-			},
-			{ "<leader>gdh", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview File History (all)" },
-			{ "<leader>gdf", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview File History (current)" },
-			{ "<leader>gdm", "<cmd>DiffviewOpen main<cr>", desc = "Diffview vs main" },
-			{ "<leader>gdt", "<cmd>DiffviewToggleFiles<cr>", desc = "Toggle Files Panel" },
-			{ "<leader>gdr", "<cmd>DiffviewRefresh<cr>", desc = "Refresh Diffview" },
+			{ "<leader>gR", "<cmd>CodeDiff<cr>", desc = "Git Diff" },
 		},
 	},
 
