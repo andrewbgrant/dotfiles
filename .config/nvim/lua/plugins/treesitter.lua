0b8025039464d1551f65fb24e94fbb48f851028b
Andrew <andrewbg8@gmail.com>
2025-12-18 09:50:28 -0500

    update treeesitter to main branch

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,22 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPost", "BufNewFile" },
+		lazy = false,
 		build = ":TSUpdate",
 		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				auto_install = true,
-				ensure_installed = { "latex" },
-				highlight = { enable = true },
-				indent = { enable = true },
+			require("nvim-treesitter").setup({
+				install_dir = vim.fn.stdpath("data") .. "/site",
+			})
+
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					pcall(vim.treesitter.start)
+					if vim.bo.filetype ~= "" then
+						pcall(function()
+							vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+						end)
+					end
+				end,
 			})
 		end,
 	},
@@ -31,32 +38,11 @@ return {
 			vim.api.nvim_set_hl(0, "treesittercontext", { bg = "#202020" })
 		end,
 	},
+
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
-		after = "nvim-treesitter",
+		branch = "main",
 		event = "VeryLazy",
-		config = function()
-			-- When in diff mode, we want to use the default
-			-- vim text objects c & C instead of the treesitter ones.
-			local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
-			local configs = require("nvim-treesitter.configs")
-			for name, fn in pairs(move) do
-				if name:find("goto") == 1 then
-					move[name] = function(q, ...)
-						if vim.wo.diff then
-							local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
-							for key, query in pairs(config or {}) do
-								if q == query and key:find("[%]%[][cC]") then
-									vim.cmd("normal! " .. key)
-									return
-								end
-							end
-						end
-						return fn(q, ...)
-					end
-				end
-			end
-		end,
 	},
 
 	{
