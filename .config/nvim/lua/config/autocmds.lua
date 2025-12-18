4945e044ed1fc44a7653b4fb3ef5b5ce73724762
Andrew <andrewbg8@gmail.com>
2025-12-18 09:50:16 -0500

    better handle external file changes

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,11 +1,14 @@
 -- Check if we need to reload the file when it changed
-vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
-	callback = function()
-		if vim.o.buftype ~= "nofile" then
-			vim.cmd("checktime")
-		end
-	end,
-})
+vim.api.nvim_create_autocmd(
+	{ "FocusGained", "TermClose", "TermLeave", "BufEnter", "WinEnter", "CursorHold", "CursorHoldI" },
+	{
+		callback = function()
+			if vim.o.buftype ~= "nofile" then
+				vim.cmd("checktime")
+			end
+		end,
+	}
+)
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
