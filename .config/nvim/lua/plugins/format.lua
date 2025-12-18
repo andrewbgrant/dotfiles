cf68f81da775bcaf19c99122a7fc36e3b222bb40
Andrew <andrewbg8@gmail.com>
2025-12-18 09:49:00 -0500

    format formatter file

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -1,30 +1,30 @@
 return {
-    "stevearc/conform.nvim",
-    opts = {},
-    event = { "BufWritePre" },
-    config = function()
-        local conform = require("conform")
-        conform.setup({
-            formatters_by_ft = {
-                lua = { "stylua" },
-                python = { "ruff_organize_imports", "ruff_format" },
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                typescriptreact = { "prettier" },
-                json = { "prettier" },
-                markdown = { "prettier" },
-                css = { "prettier" },
-                scss = { "prettier" },
-                yaml = { "prettier" },
-                html = { "prettier" },
-                sql = { "sqlfmt" },
-                sh = { "shfmt" },
-            },
+	"stevearc/conform.nvim",
+	opts = {},
+	event = { "BufWritePre" },
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "ruff_organize_imports", "ruff_format" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+				json = { "prettier" },
+				markdown = { "prettier" },
+				css = { "prettier" },
+				scss = { "prettier" },
+				yaml = { "prettier" },
+				html = { "prettier" },
+				sql = { "sqlfmt" },
+				sh = { "shfmt" },
+			},
 
-            format_on_save = {
-                timeout_ms = 3000,
-                lsp_format = "fallback",
-            },
-        })
-    end,
+			format_on_save = {
+				timeout_ms = 3000,
+				lsp_format = "fallback",
+			},
+		})
+	end,
 }
