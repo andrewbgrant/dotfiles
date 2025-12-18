b5e93aa403f2cf083a52d8f048b751947eccbb62
Andrew <andrewbg8@gmail.com>
2025-12-18 09:49:13 -0500

    migrateadd config for mini pairs

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,6 +1,15 @@
 return {
 
-	{ "nvim-mini/mini.pairs", version = false, opts = {} },
+	{
+		"nvim-mini/mini.pairs",
+		opts = {
+			modes = { insert = true, command = true, terminal = false },
+			skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+			skip_ts = { "string" },
+			skip_unbalanced = true,
+			markdown = true,
+		},
+	},
 	{ "christoomey/vim-tmux-navigator", lazy = true, event = "VimEnter" },
 
 	{
