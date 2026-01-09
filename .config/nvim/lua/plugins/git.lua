58a0df21127d0560b01f32f2a136332cfb499064
Andrew <andrewbg8@gmail.com>
2026-01-08 21:31:35 -0500

    update vscodediff to codediff

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,6 +1,6 @@
 return {
 	{
-		"esmuellert/vscode-diff.nvim",
+		"esmuellert/codediff.nvim",
 		dependencies = { "MunifTanjim/nui.nvim" },
 		keys = {
 			{ "<leader>gd", "<cmd>CodeDiff<cr>", desc = "Diff" },
