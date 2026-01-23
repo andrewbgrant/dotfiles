57593ed7f2ff7b07a6c3e297df74abf5a60f2437
Andrew <andrewbg8@gmail.com>
2026-01-22 21:17:12 -0500

    remove minipairs

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -1,15 +1,15 @@
 return {
-	{
-		"echasnovski/mini.pairs",
-		event = "InsertEnter",
-		opts = {
-			modes = { insert = true, command = true, terminal = false },
-			skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-			skip_ts = { "string" },
-			skip_unbalanced = true,
-			markdown = true,
-		},
-	},
+	-- {
+	-- 	"echasnovski/mini.pairs",
+	-- 	event = "InsertEnter",
+	-- 	opts = {
+	-- 		modes = { insert = true, command = true, terminal = false },
+	-- 		skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+	-- 		skip_ts = { "string" },
+	-- 		skip_unbalanced = true,
+	-- 		markdown = true,
+	-- 	},
+	-- },
 
 	{
 		"echasnovski/mini.surround",
