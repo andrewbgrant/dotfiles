e0b8fd05d4d21926c8300579643588ad90e5a2b6
Andrew <andrewbg8@gmail.com>
2026-08-12 19:39:14 -0400

    update format

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -1,23 +1,33 @@
+local format_on_save = {
+	timeout_ms = 3000,
+	lsp_format = "fallback",
+}
+
 return {
 	"stevearc/conform.nvim",
-	opts = {},
 	event = { "BufWritePre" },
 	config = function()
 		vim.env.PATH = vim.fn.stdpath("data") .. "/mason/bin:" .. vim.fn.expand("~/go/bin") .. ":" .. vim.env.PATH
-		local conform = require("conform")
-		conform.setup({
+		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "ruff_organize_imports", "ruff_format" },
 				go = { "goimports", "gofumpt" },
 				gomod = { "gofumpt" },
 				gowork = { "gofumpt" },
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				typescriptreact = { "prettier" },
-				json = { "prettier" },
+				javascript = { "biome", "prettier", stop_after_first = true },
+				javascriptreact = { "biome", "prettier", stop_after_first = true },
+				typescript = { "biome", "prettier", stop_after_first = true },
+				typescriptreact = { "biome", "prettier", stop_after_first = true },
+				json = { "biome", "prettier", stop_after_first = true },
+				jsonc = { "biome", "prettier", stop_after_first = true },
+				graphql = { "biome", "prettier", stop_after_first = true },
+				json5 = { "prettier" },
 				markdown = { "prettier" },
-				css = { "prettier" },
+				css = { "biome", "prettier", stop_after_first = true },
+				astro = { "prettier" },
+				svelte = { "prettier" },
+				vue = { "prettier" },
 				scss = { "prettier" },
 				yaml = { "prettier" },
 				html = { "prettier" },
@@ -26,11 +36,7 @@ return {
 				plsql = { "sqlfmt" },
 				sh = { "shfmt" },
 			},
-
-			format_on_save = {
-				timeout_ms = 3000,
-				lsp_format = "fallback",
-			},
+			format_on_save = format_on_save,
 		})
 	end,
 }
